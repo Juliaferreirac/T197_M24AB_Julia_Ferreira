3a0320042d3bbb46d07d26a503c2d85847218de5
João Pedro <82528746+jotapz@users.noreply.github.com>
2025-05-26 08:42:57 -0300

    Delete supabaseConfig.js

--- supabaseConfig.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://wxrgzlowarndbqguxevk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4cmd6bG93YXJuZGJxZ3V4ZXZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4Mzk0NDMsImV4cCI6MjA2MTQxNTQ0M30.wHiDIkIAFTmBpnyawvGH-GviM_cE0MpVTAsiiQHeRBE';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
